a3a62d40c4d40fccd4ab99b146303dd68785e395
Ruy Melo <ruyovidio@gmail.com>
2019-05-19 16:48:12 -0300

    Model Eventos adicionado

--- home_page/models.py
+++ home_page/models.py
@@ -6,11 +6,11 @@ from django.db import models
 
 class Contas(models.Model):
     foto = models.ImageField(upload_to='contas_image/')
-    conta_bancaria = models.IntegerField(max_length=10, blank=False, null=True)
-    numero_agencia = models.IntegerField(max_length=10, blank=False, null=True)
+    conta_bancaria = models.IntegerField(blank=False, null=True)
+    numero_agencia = models.IntegerField(blank=False, null=True)
     nome_titular = models.CharField(max_length=100, blank=False, null=True)
-    codigo_banco = models.IntegerField(max_length=5, blank=False, null=True)
-    cpf = models.CharField(max_length=length, blank=False, null=True)
+    codigo_banco = models.IntegerField(blank=False, null=True)
+    cpf = models.IntegerField(blank=False, null=True)
     def __str__(self):
         return self.conta_bancaria
 
@@ -21,15 +21,30 @@ class Contas(models.Model):
 class Doacao(models.Model):
 	voluntario = models.CharField(max_length=100, blank=False, null=False)
 	local = models.TextField(null=False, blank=False)
-	dias = models.CharField(max_lenght=50 null=True, blank=True)
+	dias = models.CharField(max_length=50, null=True, blank=True)
 	horario = models.IntegerField(null=True, blank=True)
 	foto = models.ImageField(upload_to="doacao_image/")
 
 	def __str__(self):
 		return self.local
 
-	class Meta{
+	class Meta:
 		verbose_name_plural = 'Pontos de arrecadação'
 		verbose_name = 'Ponto de arrecadação'
-	}
+
 #Model Eventos
+class Eventos(models.Model):
+	foto = models.ImageField(upload_to="evento_image/")
+	local = models.TextField(null=False, blank=False)
+	dia = models.CharField(max_length=50, null=True, blank=True)
+	horario = models.IntegerField(null=True, blank=True)
+	infoEvento = models.TextField(null=False, blank=False)
+	infoAnimais = models.TextField(null=False, blank=False)
+
+	def __str__(self):
+		return self.local
+
+	class Meta:
+		verbose_name_plural = 'Eventos'
+		verbose_name = 'Evento'
+
