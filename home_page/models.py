716eb1ef55259f8b1723ba1f168fd4fa34b3152f
Ruy Melo <ruyovidio@gmail.com>
2019-05-19 16:14:51 -0300

    Model Doacao

--- home_page/models.py
+++ home_page/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+#Model Bancos
+
+#Model Doacao
+class Doacao(models.Model):
+	voluntario = models.CharField(max_length=100, blank=False, null=False)
+	local = models.TextField(null=False, blank=False)
+	dias = models.CharField(max_lenght=50 null=True, blank=True)
+	horario = models.IntegerField(null=True, blank=True)
+	foto = models.ImageField(upload_to="doacao_image/")
+
+	def __str__(self):
+		return self.local
+
+	class Meta{
+		verbose_name_plural = 'Pontos de arrecadação'
+		verbose_name = 'Ponto de arrecadação'
+	}
+
+#Model Eventos
