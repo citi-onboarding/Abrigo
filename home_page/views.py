9578c4296c5d7ba3c98d67e4697cc84adf8412f4
Pedro Reis <pgsr@cin.ufpe.br>
2019-05-19 11:01:06 -0300

    pasta de template realocada

--- home_page/views.py
+++ home_page/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.urls import reverse_lazy
+from templated_email import send_templated_mail
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home_page/home.html')
+
+class ContatoView(generic.TemplateView):
+    template_name = 
