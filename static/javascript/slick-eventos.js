28326610d543c8c7e538a970cf650516da8c8dfe
Ruy Melo <ruyovidio@gmail.com>
2019-05-28 13:11:03 -0300

    Carrossel estático

---
+++ static/javascript/slick-eventos.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carrossel-eventos').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        arrows: false,
+        centerMode: true,
+        variableWidth: true
+    });
+});
