d43a6d7b6bc5c803a4a9293823e2bc1a9f8a60ab
Pedro Gabriel dos Santos Reis <pgsr@cin.ufpe.br>
2019-06-04 22:37:49 -0300

    adicionado os alts de Como Ajudar, Header e Footer

--- static/js/functions.js
+++ static/js/functions.js
@@ -5,7 +5,6 @@ function showMenu() {
 
 let caModal = document.querySelector('.ca-banco-info');
 let caBtn = document.querySelector('.ca-modal');
-var count = 0;
 
 caBtn.onclick = function(){
     caModal.style.display = "block";
@@ -14,7 +13,6 @@ caBtn.onclick = function(){
 window.addEventListener("click", function(event){
     console.log(event.target);
     if(event.target != caModal && event.target != caBtn && screen.width>1024){
-        console.log('IGUAL!!!!!!');
         caModal.style.display = "none";
     }
 });
