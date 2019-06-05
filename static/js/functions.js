5b4b7eb919c3a3d42b60f0275b0dde74a2d74c17
Pedro Gabriel dos Santos Reis <pgsr@cin.ufpe.br>
2019-06-05 00:01:53 -0300

    modal ajustado

--- static/js/functions.js
+++ static/js/functions.js
@@ -3,16 +3,18 @@ function showMenu() {
     x.classList.toggle('change');
 }
 
+
+let caSlick = document.querySelector('.slick-infos');
 let caModal = document.querySelector('.ca-banco-info');
 let caBtn = document.querySelector('.ca-modal');
 
 caBtn.onclick = function(){
-    caModal.style.display = "block";
+    caSlick.style.display = "block";
 }
 
 window.addEventListener("click", function(event){
     console.log(event.target);
-    if(event.target != caModal && event.target != caBtn && screen.width>1024){
-        caModal.style.display = "none";
+    if(event.target != caModal && event.target != caBtn && screen.width>1024 && event.target != caSlick){
+        caSlick.style.display = "none";
     }
 });
